13993499eebaf75274a29383ab5413752d637a2b
Emanuele <emanuele.serrao.92@gmail.com>
2024-01-11 19:31:33 +0100

    U

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -48,21 +48,52 @@ function App() {
 
   return (
     <div className="App">
-      {routes.map((x: IAppButton, i: number) => {
-        return (
-          <AppButton
-            active={openPage === x.path}
-            emit={() => {
-              setOpenPage(x.path);
-            }}
-            key={i}
-            target={x}
-          />
-        );
-      })}
-      <div>
-        <iframe className="main-iframe" src={openPage} />
+      <div className="toolbar">
+        <div
+          className="toolbar-buttons-logo"
+          onClick={() => {
+            setOpenPage("");
+          }}
+        >
+          Nikasi Logo
+        </div>
+        <div className="toolbar-buttons">
+          {routes.map((x: IAppButton, i: number) => {
+            return (
+              <AppButton
+                active={openPage === x.path}
+                emit={() => {
+                  setOpenPage(x.path);
+                }}
+                key={i}
+                target={x}
+              />
+            );
+          })}
+        </div>
       </div>
+      {openPage === "" && (
+        <div className="intro-text">
+          <h2>Benvenuto in Nikasi</h2>
+          <div>Sistema sviluppato da bla bla</div>
+          <div>Copiright bla bla</div>
+          <div>Copiright bla bla</div>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </div>
+      )}
+      {openPage !== "" && (
+        <div>
+          <iframe className="main-iframe" src={openPage} />
+        </div>
+      )}
     </div>
   );
 }
